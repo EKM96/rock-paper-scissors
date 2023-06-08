cd8f3a324bde5f955d39a77b660b08ffa0ba8191
EKM96 <124158593+EKM96@users.noreply.github.com>
2023-06-08 14:14:31 -0600

    Fix 'playRound' conditional function to work correctly when called multiple times in for loop

--- javascript.js
+++ javascript.js
@@ -1,26 +1,35 @@
-let computerSelection = getComputerChoice ();
-
-
-// Create a variable that prompts the user to choose between "rock", "paper" or "scissors" and converts the prompt to lower case
-let playerSelection = prompt('Choose Rock, Paper or Scissors', "").toLowerCase();
-
-
-// Create a function that randomly returns either "rock", "paper", or "scissors"
-function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors'];
-
-    // get random index value
-    let randomIndex = Math.floor(Math.random()*choice.length);
-
-    // get random item
-    let item = choice[randomIndex];
-    return item;
-}
+let computerSelection,
+    playerSelection;
 
 
 // Create a function that takes "computerSelection" and "playerSelection" as parameters and  
 // returns a string that declares the result (win, loss or tie) of the round.
 function playRound (playerSelection, computerSelection) {
+
+    playerSelection = getPlayerChoice ();
+    computerSelection = getComputerChoice ();
+    
+
+    // Create a function that prompts the user to choose between "rock", "paper" or "scissors" 
+    // and converts the prompt to lower case
+    function getPlayerChoice () {
+        return prompt('Choose Rock, Paper or Scissors', "").toLowerCase();
+    }
+
+
+    // Create a function that randomly returns either "rock", "paper", or "scissors"
+    function getComputerChoice () {
+        let choice = ['rock', 'paper', 'scissors'];
+
+        // get random index value
+        let randomIndex = Math.floor(Math.random()*choice.length);
+
+        // get random item
+        let item = choice[randomIndex];
+        return item;
+    }
+
+
     if (playerSelection === "rock" && computerSelection === "scissors") {
         let result = console.log(`You win this round! \n${playerSelection} beats ${computerSelection}`);
         return result;
@@ -44,15 +53,13 @@ function playRound (playerSelection, computerSelection) {
         return result;
     }
     
+} 
+
+    
+
+for (let i=0; i<5; i++) {
+    playRound (playerSelection, computerSelection);
 }
-
-
-
-// printChoicesMessagge();
-playRound(playerSelection, computerSelection);
-
-
-
 
 
 
